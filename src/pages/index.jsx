8706d38fdc609a9455796a2b3a72589e1fb67442
ochom <ochomrichard752@gmail.com>
2024-08-07 09:57:31 +0300

    remove unsued files

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,7 +18,7 @@ export default function Index() {
       </div>
       <h1>Vite + React</h1>
       <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+        <button onClick={() => setCount(prev => prev + 1)}>count iss {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
